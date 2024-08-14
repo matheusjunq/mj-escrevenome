5a2831c8563eda4406dfba5eec7b38eb2989f4a3
matheusjunq <00001086687292sp@al.educacao.sp.gov.br>
2024-08-14 15:48:14 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,47 +1,15 @@
-//variaveis da bolinha 
-let xBolinha = 300;
-let yBolinha = 200;
-let Diametro = 22;
-let Raio = Diametro / 2;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
 
-//velocidadae
-let  velocidadexBolinha = 10;
-let  velocidadeyBolinha = 10;
-
-
-function setup() {
-  createCanvas(600, 400);
-}
-
-function draw() {
-  background("black");
-  mostraBolinha();
-  movimentaBolinha();
-  verificacolisaoBorda();
- 
-  
-}
-
- function mostraBolinha(){ 
- circle(xBolinha, yBolinha, Diametro); 
- }
- 
-function movimentaBolinha(){ 
- xBolinha += velocidadexBolinha;
- yBolinha += velocidadeyBolinha; 
-}
-
-
-function verificacolisaoBorda(){ 
- 
-  if(xBolinha + Raio > width || xBolinha - Raio< 0 ){ 
-  velocidadexBolinha *= -1;
-  }
-  { 
-   if (yBolinha + Raio > height ||    
-      yBolinha - Raio < 0 ) 
-    velocidadeyBolinha *= -1;
-    
-  }
-
-}
+  </head>
+  <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
+  </body>
+</html>
